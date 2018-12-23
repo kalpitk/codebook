b4223f3f6baf657fdb71e7a3cea86ef4b4629197
Kalpit <kkothari1999@gmail.com>
2018-12-24 00:01:14 +0530

    Update modular.cpp

--- modular.cpp
+++ modular.cpp
@@ -5,24 +5,8 @@
 typedef vector<int> VI;
 typedef pair<int,int> PII;
 
-// return a % b (positive value)
-int mod(int a, int b) {
-  return ((a%b)+b)%b;
-}
-
-// computes gcd(a,b)
-int gcd(int a, int b) {
-  int tmp;
-  while(b){a%=b; tmp=a; a=b; b=tmp;}
-  return a;
-}
-
-// computes lcm(a,b)
-int lcm(int a, int b) {
-  return a/gcd(a,b)*b;
-}
-
-// returns d = gcd(a,b); finds x,y such that d = ax + by
+// returns d = gcd(a,b); 
+// finds x,y such that d = ax + by
 int extended_euclid(int a, int b, int &x, int &y) {  
   int xx = y = 0;
   int yy = x = 1;
