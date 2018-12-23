f7a1f68c017b2574ca51c24a3b528a8adec581c3
Kalpit <kkothari1999@gmail.com>
2018-12-23 16:00:00 +0530

    refactor: remove ftype CHT

--- chtrick(offline).cpp
+++ chtrick(offline).cpp
@@ -1,20 +1,19 @@
 //this can be used only in offline manner as add_line must be done in increasing order of slopes of the lines
-typedef int ftype;
-typedef complex<ftype> point;
+typedef complex<int> point;
 #define x real
 #define y imag
 
-ftype dot(point a, point b) {
+int dot(point a, point b) {
     return (conj(a) * b).x();
 }
 
-ftype cross(point a, point b) {
+int cross(point a, point b) {
     return (conj(a) * b).y();
 }
 
 vector<point> hull, vecs;
 
-void add_line(ftype k, ftype b) {
+void add_line(int k, int b) {
     point nw = {k, b};
     while(!vecs.empty() && dot(vecs.back(), nw - hull.back()) < 0) {
         hull.pop_back();
@@ -26,7 +25,7 @@ void add_line(ftype k, ftype b) {
     hull.push_back(nw);
 }
 
-int get(ftype x) {
+int get(int x) {
     point query = {x, 1};
     auto it = lower_bound(vecs.begin(), vecs.end(), query, [](point a, point b) {
         return cross(a, b) > 0;
